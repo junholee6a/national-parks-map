3f9ac6219170381856b9d2e60a8bf3a6c3df00f3
Junho Lee <junholee6a@gmail.com>
2022-11-23 18:43:23 -0500

    Create a simple DeckGL LineLayer
    Copied code from https://deck.gl/docs/get-started/using-with-react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+/*
+References:
+- Followed this tutorial: https://deck.gl/docs/get-started/using-with-react
+*/
 
+import React from 'react';
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [
+  {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
+];
+
+// DeckGL react component
 function App() {
-  return (
-    <div>
-      <p>Testing</p>
-    </div>
-  );
+  const layers = [
+    new LineLayer({id: 'line-layer', data})
+  ];
+
+  return <DeckGL
+    initialViewState={INITIAL_VIEW_STATE}
+    controller={true}
+    layers={layers} />
 }
 
 export default App;
